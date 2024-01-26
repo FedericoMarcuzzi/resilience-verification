14e579381d7efc78e2b4c2adc937d0c8bd4968c1
Federico Marcuzzi <36448349+FedericoMarcuzzi@users.noreply.github.com>
2024-01-26 14:52:50 +0100

    Update cod-rna.sh

--- exp_scripts/Figure3/cod-rna.sh
+++ exp_scripts/Figure3/cod-rna.sh
@@ -4,4 +4,4 @@
 g++ -O3 -o ../../src/robustness_varying_n_synthetic_datasets ../../src/robustness_varying_n_synthetic_datasets.cpp -I../../src/include
 
 #generate rob value for each sythetic dataset
-../../src/robustness_varying_n_synthetic_datasets ../../exp/cod_rna/models/treant/treant_cr_5_3_sse_1_0.07_left.json ../../exp/cod_rna/atk/atk_cr_0.07_l.json 1 ../../exp/cod_rna/ test_set_cr 0.01
+../../src/robustness_varying_n_synthetic_datasets ../../exp/cod_rna/models/rf/rf_cr_5_3.json ../../exp/cod_rna/atk/atk_cr_0.07_l.json 1 ../../exp/cod_rna/ test_set_cr 0.01
